71a39cc7372b344cda006d6e685fb45a8ddeab25
Hmzbo <hammzaboulahia@gmail.com>
2021-07-23 19:27:42 +0100

    Add files for Heroku

--- App_KCP.py
+++ App_KCP.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from Predict_page import show_pred_page
-from Data_EDA import show_EDA_page
+from App_Predict_page import show_pred_page
+from App_Data_EDA import show_EDA_page
 
 page = st.sidebar.selectbox('Page', options=('Data EDA','Predict'))
 
